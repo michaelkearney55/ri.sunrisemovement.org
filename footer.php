7edcc49d8476e99ba8a92be6abb8129fdae920fa
Michael Kearney <michaelkearney55@gmail.com>
2019-10-25 21:26:08 -0400

    Created index.html from index.php

--- footer.php
+++ footer.php
@@ -1,7 +1,8 @@
 <footer>
     <div class="background gray">
         <div class="container">
-            <div class="wide">
+            <div class="wide center-self">
+                <h2>This is the footer.</h2>
             </div>
         </div>
     </div>
